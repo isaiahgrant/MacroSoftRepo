aa969df33208adc507530aedc86b1211ecfeef86
austinwmj <austinwmj@yahoo.com>
2014-05-14 15:32:14 -0700

    Maze Class
    Here is the maze class. I included two different prints, I think working
    with an array will be much easier.

---
+++ Trivia_Maze_Project/CurrentWorkingDirectory/TriviaMaze/src/Maze.java
@@ -0,0 +1,116 @@
+/**
+ * @author ajohnston
+ *
+ * Let me know what you think of the drawMaze methods.
+ * Pretty much everything in this class will get more complicated as 
+ * the rest of our project does.
+ */
+public class Maze {
+
+	Room[][] rooms;
+	Room entrance;
+	Room exit;
+	Door current_Door;
+	Player player;
+	
+	public Maze(Player p1)
+	{
+		this.rooms = generateMaze();
+		this.current_Door = null;
+		this.entrance = rooms[0][0];
+		this.exit = rooms[5][5];
+		this.player = p1;
+	}
+	
+	Room[][] generateMaze()
+	{
+		Room[][] rooms = new Room[5][5];
+		return rooms;
+	}
+	
+	boolean isValidMove(Direction direct)
+	{
+		int x = this.player.getPlayerLocation().getColumn();
+		int y = this.player.getPlayerLocation().getRow();
+		if(x == 0 && direct.equals(Direction.LEFT))
+			return false;
+		else if(x == 5 && direct.equals(Direction.Right))
+			return false;
+		else if(y == 0 && direct.equals(Direction.Up))
+			return false;
+		else if(y == 5 && direct.equals(Direction.DOWN))
+			return false;
+		return true;
+	}
+	
+	boolean isValidAnswer(String answer)
+	{
+		return this.current_Door.getTriviaItem().getAnswer().equals(answer);
+	}
+	
+	void movePlayer(Direction direct)
+	{
+		if(direct == Direction.UP)
+			this.player.getPlayerLocation().decreaseRowByOne();
+		if(direct == Direction.DOWN)
+			this.player.getPlayerLocation().increaseRowByOne();
+		if(direct == Direction.LEFT)
+			this.player.getPlayerLocation().decreaseColumnByOne();
+		if(direct == Direction.RIGHT)
+			this.player.getPlayerLocation().increaseColumnByOne();		
+	}
+	
+	boolean isSolvable()
+	{
+		return true;
+	}
+	
+	//thinking doing this with an array would be easier and also less prone to errors.
+	void drawMaze()
+	{
+		System.out.println(". . . . . . . . . . .");
+		for(int i = 1; i < 5; i++)
+		{
+			if(this.player.getPlayerLocation().getRow() == i)
+			{
+				for(int j = 0; j < 5; j++)
+				{
+					if(this.player.getPlayerLocation().getColumn() == j)
+						System.out.print(". P ");
+					else
+						System.out.print(".   ");
+				}
+				System.out.println(".");
+			}
+			else
+				System.out.println(".   .   .   .   .   .");
+			System.out.println(". . . . . . . . . . .");
+		}
+	}
+	
+	//this is my implementation with an array
+	void drawMazeArray()
+	{
+		char[][] mazeAra = new char[5][5];
+		for(int i = 0; i < 5; i++)
+		{
+			for(int j = 0; j < 5; j++)
+				mazeAra[i][j] = '.';
+		}
+		mazeAra[0][0] = 'E';
+		mazeAra[5][5] = 'X';
+		mazeAra[this.player.getPlayerLocation().getColumn()][this.player.getPlayerLocation().getRow()] = 'P';
+		
+		System.out.println(". . . . . . . . . . .")
+		for(int i = 0; i < 5; i ++)
+		{
+			System.out.print(".")
+			for(int j = 0; j < 5; j++)
+			{
+				System.out.print(" " + mazeAra[i][j] + " .");
+			}
+			System.out.println("\n. . . . . . . . . . .")
+		}
+	}
+	
+}
