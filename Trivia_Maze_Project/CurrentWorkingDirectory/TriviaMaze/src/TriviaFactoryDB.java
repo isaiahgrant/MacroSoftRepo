da3825341f9776d545a3c642509fde1dfabd3592
astaeheli <astaeheli@eagles.ewu.edu>
2014-05-15 19:59:36 -0700

    Database driven trivia factory
    Triviadb.db -> our trivia database. currently holds the 24 entries in
    TriviaFile.txt
    
    TriviaFactoryDB.java -> Same functionality as the file driven
    triviafactory but connects to and queries from triviadb.db
    
    TriviaMazeDriver.java -> Basic tester that constructs a TriviaFactoryDB
    and produces a new TriviaItem each time getQuestion() is called.
    
    If all goes well then you should be able to run TriviaMazeDriver without
    setting anything up.

---
+++ Trivia_Maze_Project/CurrentWorkingDirectory/TriviaMaze/src/TriviaFactoryDB.java
@@ -0,0 +1,83 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TriviaFactoryDB 
+{
+	private Connection connection = null;
+	private ResultSet resultSet = null;
+	private Statement statement = null;
+	
+	public TriviaFactoryDB()
+	{
+		
+		try
+		{
+			Class.forName("org.sqlite.JDBC");
+		}catch(Exception e)
+		{
+			System.out.println("Unable to load driver class");
+			e.printStackTrace();
+		}
+		
+		
+		try
+		{
+			connection = DriverManager.getConnection("jdbc:sqlite:triviadb.db");
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery("SELECT * FROM trivia;");
+		}catch(Exception e)
+		{
+			System.out.println("Unable to make a connection");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public TriviaItem getTriviaItem()
+	{
+		TriviaItem temp = null;
+		
+		try
+		{
+			resultSet.next();
+			if(resultSet.getString(2).equalsIgnoreCase("tf"))
+			{
+				temp = new TrueFalse(resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5));
+				return temp;
+			}
+			
+			if(resultSet.getString(2).equalsIgnoreCase("mc"))
+			{
+				temp = new MultiChoice(resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8), resultSet.getString(9));
+				return temp;
+			}
+			
+		}catch(SQLException e)
+		{
+			System.out.println("Reached the end of data entries.  Generating TriviaItems with default values");
+			temp = new MultiChoice();
+			return temp;
+		}
+		return null;
+	}
+	
+	
+}//end TriviaFactoryDB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Trivia_Maze_Project/CurrentWorkingDirectory/TriviaMaze/src/TriviaMazeDriver.java
@@ -0,0 +1,24 @@
+
+
+
+public class TriviaMazeDriver 
+{
+
+	public static void main(String[] args) 
+	{
+		int i;
+		TriviaItem temp = null;
+		TriviaFactoryDB factory = new TriviaFactoryDB();
+		
+
+		for(i = 0; i < 26; i++)
+		{
+			temp = factory.getTriviaItem();
+			System.out.println(temp.askQuestion());
+			System.out.println();
+		}
+		
+		
+	}
+
+}
