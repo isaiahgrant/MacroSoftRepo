43943b3764580dfdd95c5e629d2ec3e1694c0419
astaeheli <astaeheli@eagles.ewu.edu>
2014-05-21 10:00:27 -0700

    TriviaFactory randomization implemented.
    Now selects a random row from the database when getTriviaItem is called.
    Works its way through the entire database without repeating a TriviaItem
    and if all of the database entries have been used it generates "Default"
    questions.

--- Trivia_Maze_Project/CurrentWorkingDirectory/TriviaMaze/src/TriviaFactoryDB.java
+++ Trivia_Maze_Project/CurrentWorkingDirectory/TriviaMaze/src/TriviaFactoryDB.java
@@ -3,19 +3,28 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class TriviaFactoryDB 
 {
 	private Connection connection = null;
 	private ResultSet resultSet = null;
 	private Statement statement = null;
+	private Random rnd = null;
+	private int maxRows;
+	private ArrayList<Integer> usedRows;
 	
 	public TriviaFactoryDB()
 	{
+		usedRows = new ArrayList<Integer>();
+		rnd = new Random(System.currentTimeMillis());
 		
 		try
 		{
 			Class.forName("org.sqlite.JDBC");
+			connection = DriverManager.getConnection("jdbc:sqlite:triviadb.db");
+			statement = connection.createStatement();
 		}catch(Exception e)
 		{
 			System.out.println("Unable to load driver class");
@@ -25,9 +34,11 @@ public class TriviaFactoryDB
 		
 		try
 		{
-			connection = DriverManager.getConnection("jdbc:sqlite:triviadb.db");
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery("SELECT * FROM trivia;");
+			resultSet = statement.executeQuery("SELECT MAX(ID) FROM TRIVIA;");
+			maxRows = resultSet.getInt(1);
+			//System.out.println("maxRows is: " + maxRows);
+			
+			//resultSet = statement.executeQuery("SELECT * FROM trivia;");
 		}catch(Exception e)
 		{
 			System.out.println("Unable to make a connection");
@@ -39,7 +50,43 @@ public class TriviaFactoryDB
 	public TriviaItem getTriviaItem()
 	{
 		TriviaItem temp = null;
+		Integer tempRnd;
 		
+		if(usedRows.size() == 24)
+		{
+			return new MultiChoice();
+		}
+		
+				
+		tempRnd = Math.abs((rnd.nextInt() % maxRows)) + 1;
+		if(!usedRows.contains(tempRnd))
+		{
+			usedRows.add(tempRnd);
+		}
+		
+		else
+		{
+			while(usedRows.contains(tempRnd))
+			{
+				tempRnd = Math.abs((rnd.nextInt() % maxRows)) + 1;
+			}
+			
+			usedRows.add(tempRnd);
+		}
+		
+		try
+		{
+			resultSet = statement.executeQuery("SELECT * FROM TRIVIA WHERE ID = " + tempRnd + ";");
+			
+		}catch(Exception e)
+		{
+			System.err.println("There was an erro querying the database.");
+		}
+		
+		//uncomment this print statement if you want to see the ID number of each question.
+		//System.out.println(tempRnd);
+		
+					
 		try
 		{
 			resultSet.next();
@@ -54,28 +101,27 @@ public class TriviaFactoryDB
 				temp = new MultiChoice(resultSet.getString(2), resultSet.getString(3), resultSet.getString(4), resultSet.getString(5), resultSet.getString(6), resultSet.getString(7), resultSet.getString(8), resultSet.getString(9));
 				return temp;
 			}
-			
+			//System.out.print(resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getString(3) + " " + resultSet.getString(4) + "\n");
 		}catch(SQLException e)
 		{
 			System.out.println("Reached the end of data entries.  Generating TriviaItems with default values");
-			temp = new MultiChoice();
-			return temp;
+			return new MultiChoice();
 		}
 		return null;
 	}
 	
-	
 	public void closeFactory()
 	{
-		try
+		try 
 		{
 			statement.close();
+			resultSet.close();
 			connection.close();
-		}catch(Exception e)
+		} catch (SQLException e) 
 		{
-			System.out.println("Unable to close factory");
+			System.err.println("Error closing SQL objects");
+			e.printStackTrace();
 		}
-				
 	}
 	
 	
