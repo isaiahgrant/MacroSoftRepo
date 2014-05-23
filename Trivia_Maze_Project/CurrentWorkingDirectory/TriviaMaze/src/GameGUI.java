b6bcccc4273872267d791aa27a602ff35bb7854e
Andrey Melnikov <andreypmelnikov@eagles.ewu.edu>
2014-05-22 18:18:52 -0700

    Added temporary code so mazeGUI can draw maze
    *Added temporary code to populate the rooms/doors so that the MazeGUI
    can draw itself

--- Trivia_Maze_Project/CurrentWorkingDirectory/TriviaMaze/src/GameGUI.java
+++ Trivia_Maze_Project/CurrentWorkingDirectory/TriviaMaze/src/GameGUI.java
@@ -48,11 +48,18 @@ public class GameGUI extends Game implements ActionListener, GamePortion
 	//END DEBUGING MATERIAL
 	
 	public GameGUI(int width, int height, Difficulty difficulty, ActionListener listener)
-//	public GameGUI(int width, int height, Difficulty difficulty)
 	{
 		super();
 		
 		this.gameMaze = new MazeGUI(new Player("Sven"));
+		
+		//TEMP CODE
+		/* This should probably be handled in some fashion within the Maze class (maybe new constructor?) */
+		this.mazeBuilder = new MazeBuilder(Difficulty.EASY);
+		this.gameMaze.rooms = this.mazeBuilder.getNewMaze();
+		this.gameMaze.entrance = this.gameMaze.rooms[0][0];
+		this.gameMaze.exit = this.gameMaze.rooms[this.gameMaze.rooms.length - 1][this.gameMaze.rooms[0].length - 1];
+		//END TEMP CODE
 
 		this.setUpCanvas(width, height);
 		this.setUpTextInputOutput();
