3e94a86fc5585ae5233100fc7ba765d6aadfea0c
Andrey Melnikov <andrey.melnikov@eagles.ewu.edu>
2014-06-05 11:58:05 -0700

    Minor code clean up

--- Trivia_Maze_Project/CurrentWorkingDirectory/TriviaMaze/src/Maze.java
+++ Trivia_Maze_Project/CurrentWorkingDirectory/TriviaMaze/src/Maze.java
@@ -1,13 +1,4 @@
-import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 
 /**
  * @author ajohnston
